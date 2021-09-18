6c0dfa99d6ce09510188b879b08da58bc45c0776
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2021-09-19 03:19:13 +0530

    import nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './App.css'
-import { Switch, route, Link } from 'react-router-dom'
+// import { Switch, route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='App'>
-      <div className='navbar'></div>
+      <div className='navbar'>
+        <Navbar />
+      </div>
       <div className='main'></div>
       <div className='footer'></div>
     </div>
