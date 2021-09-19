f1c062fd6772b3acca7d9b4f13f9e668b7c55231
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2021-09-19 22:44:40 +0530

    news service

---
+++ src/services/CryptoNews.js
@@ -0,0 +1,27 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoNewsHeaders = {
+  'x-bingapis-sdk': 'true',
+  'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+  'x-rapidapi-key': 'a9d3011a5dmsh20902063431fdbbp17fc11jsn845f285cb552'
+}
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+
+const createRequest = url => ({
+  url,
+  headers: cryptoNewsHeaders
+})
+
+export const CryptoNewsApi = createApi({
+  reducerPath: 'CryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: builder => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        )
+    })
+  })
+})
