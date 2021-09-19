75ed1d6bc9a9daef4bd13d7f7fdd522e67a4bd76
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2021-09-19 17:05:01 +0530

    add layout in home

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import millify from 'millify'
+import { Typography, Row, Col, Statistic } from 'antd'
+
+const { Title } = Typography
 
 const Homepage = () => {
-    return (
-        <div>
-            Homepage
-        </div>
-    )
+  return (
+    <>
+      <Title level={2} className='heading'>
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title='Total Cryptocurrencies' value='5' />
+        </Col>
+        <Col span={12}>
+          <Statistic title='Total Cryptocurrencies' value='5' />
+        </Col>
+        <Col span={12}>
+          <Statistic title='Total Cryptocurrencies' value='5' />
+        </Col>
+        <Col span={12}>
+          <Statistic title='Total Cryptocurrencies' value='5' />
+        </Col>
+        <Col span={12}>
+          <Statistic title='Total Cryptocurrencies' value='5' />
+        </Col>
+      </Row>
+    </>
+  )
 }
 
 export default Homepage
