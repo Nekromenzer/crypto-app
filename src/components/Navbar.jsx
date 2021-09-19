87518c29e50f32b03dcd7a296e142a6fc260ad66
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2021-09-19 16:20:31 +0530

    nav bar menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 import {
   HomeOutlined,
   MoneyCollectOutlined,
-  BuldOutlined,
+  BulbOutlined,
   FundOutlined,
   MenuOutlined
 } from '@ant-design/icons'
-import  icon  from '../Images/logo.png'
+import icon from '../Images/logo.png'
 
 const Navbar = () => {
   return (
@@ -18,10 +18,22 @@ const Navbar = () => {
         <Typography.Title level={2} className='logo'>
           <Link>CryptoVerse</Link>
         </Typography.Title>
-        {/* <Button className="menu-control-container">
-
-          </Button> */}
+       
       </div>
+      <Menu theme='dark'>
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to='/'>Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />}>
+            <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined />}>
+            <Link to='/exchanges'>Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined />}>
+            <Link to='/news'>News</Link>
+          </Menu.Item>
+        </Menu>
     </div>
   )
 }
