227001aae514ebd4c4d705293b839091b2784fa7
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2021-09-19 17:25:51 +0530

    wrap with redux

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import 'antd/dist/antd.css'
+import { store } from './app/store'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
