88332dd61415e610914409e87fcba07a98876d9d
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2021-10-07 21:21:56 +0530

    mkae menu responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Button, Menu, Avatar, Typography } from 'antd'
 import { Link } from 'react-router-dom'
 import {
@@ -11,6 +11,24 @@ import {
 import icon from '../Images/logo.png'
 
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = React.useState(true)
+  const [screenSize, setScreenSize] = React.useState(null)
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth)
+    window.addEventListener('resize', handleResize)
+    handleResize()
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  useEffect(() => {
+    if (screenSize < 768) {
+      setActiveMenu(false)
+    } else {
+      setActiveMenu(true)
+    }
+  }, [screenSize])
+
   return (
     <div className='nav-container'>
       <div className='logo-container'>
@@ -18,9 +36,15 @@ const Navbar = () => {
         <Typography.Title level={2} className='logo'>
           <Link>CryptoVerse</Link>
         </Typography.Title>
-       
       </div>
-      <Menu theme='dark'>
+      <Button
+        className='menu-control-container'
+        onClick={() => setActiveMenu(!activeMenu)}
+      >
+        <MenuOutlined />
+      </Button>
+      {activeMenu && (
+        <Menu theme='dark'>
           <Menu.Item icon={<HomeOutlined />}>
             <Link to='/'>Home</Link>
           </Menu.Item>
@@ -34,6 +58,7 @@ const Navbar = () => {
             <Link to='/news'>News</Link>
           </Menu.Item>
         </Menu>
+      )}
     </div>
   )
 }
